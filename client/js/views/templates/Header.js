2d725792c73a6981695e074bff58fc6e74149663
AlexCadigan <cadiganalexander@gmail.com>
2018-02-18 23:34:33 -0500

    Finished header page - fonts still not working

--- client/js/views/templates/Header.js
+++ client/js/views/templates/Header.js
@@ -1,11 +1,11 @@
 module.exports = function( { model } ) {
 	const navOptions = model.forEach(datum => `<span> ${ this.CapitalizeFirstLetter(datum) } </span>`)
 	return `<nav>
-	<div> <a href = "InsertURLToAboutUS"> ABOUT US </a> </div>
-	<div> <a href = "InsertURLToWhereToFindUs"> WHERE TO FIND US </a> </div>
-	<div> <a class = "hCurrent" href = "InsertURLToFutureDaysFarm"> FUTURE DAYS FARM </a> </div>
-	<div> <a href = "InserURLToTheBlog"> THE BLOG </a> </div>
-	<div> <a href = "InsertURLToOurOfferings"> OUR OFFERINGS </a> </div> <br>
-	<div class = "hLogo"> <img src = "https://storage.googleapis.com/five-gallon/FutureDaysFarmLogo.svg" /> </div>
+	<div class = "hDiv"> <a class = "hLink" href = "InsertURLToAboutUS"> ABOUT US </a> </div>
+	<div class = "hDiv"> <a class = "hLink" href = "InsertURLToWhereToFindUs"> WHERE TO FIND US </a> </div>
+	<div class = "hDiv"> <a class = "hCurrent" href = "InsertURLToFutureDaysFarm"> FUTURE DAYS FARM </a> </div>
+	<div class = "hDiv"> <a class = "hLink" href = "InserURLToTheBlog"> THE BLOG </a> </div>
+	<div class = "hDiv"> <a class = "hLink" href = "InsertURLToOurOfferings"> OUR OFFERINGS </a> </div>
+	<div class = "hLogo"> <img class = "hImg" src = "https://storage.googleapis.com/five-gallon/FutureDaysFarmLogo.svg" /> </div>
 	</nav>`
 }
