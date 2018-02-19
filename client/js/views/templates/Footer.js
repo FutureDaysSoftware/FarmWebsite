b52433278c9ce88f1d3f69a252ec89b14e1767db
AlexCadigan <cadiganalexander@gmail.com>
2018-02-18 23:48:48 -0500

    Finished footer page - fonts still not working

--- client/js/views/templates/Footer.js
+++ client/js/views/templates/Footer.js
@@ -1,88 +1,11 @@
-/**
-* Project: Future Days Farm Website
-* Created By: Chris Baron
-* Date Last Modified: 1/19/2018 by Alex Cadigan
-* Description: This file builds the footer elements on the Farm Website homepage
-*/
-
-module.exports = function() 
-{ 
-    return `<footer id = "footerBody">
-
-    <!-- Future Days Farm Info -->
-    <div id = "footerTitle" class = "footer">
-
-        FUTURE DAYS FARM
-
-    </div>
-
-    <div class = "footer">
-
-        2123 Tiny Road
-        <br>
-        Town Name, Michigan 33344        
-
-    </div>
-
-    <br>
-
-    <div class = "footer">
-
-        <a id = "footerLink" href = "mailto:Info@FutureDaysFarm.com"> Info@FutureDaysFarm.com </a>
-        <br>
+module.exports = function() { 
+    return `<footer>
+    <div class = "fTitle"> FUTURE DAYS FARM </div>
+    <div class = "fInfo"> 
+        2123 Tiny Road <br> Town Name, Michigan 33344 <br> <br>
+        <a class = "fLink" href = "mailto:Info@FutureDaysFarm.com"> Info@FutureDaysFarm.com </a> <br>
         (333) 323-8899
-
-    </div>
-
-    <br>
-
-    <!-- Copyright -->
-    <div id = "footerBottom" class = "footer">
-
-        Copyright ${ new Date().getFullYear() } FutureDays Software
-
-    </div>
-
-    <style>
-
-        @import url('https://fonts.googleapis.com/css?family=Roboto');
-        @import url('https://fonts.googleapis.com/css?family=Arvo');
-
-        /**
-        * Body of footer
-        */
-        #footerBody
-        {
-            background-color: #C49542;
-        }
-
-        /**
-        * Future Days Farm Info
-        */
-        .footer
-        {
-            text-align: center;
-            font-family: Roboto;
-            color: #231F20;
-        }
-        #footerTitle
-        {
-            padding-top: 40px;
-            font-family: Arvo;
-            color: white;
-            font-weight: bold;
-        }
-        #footerLink
-        {
-            color: #231F20;
-            text-decoration: none;
-        }
-        #footerBottom
-        {
-            padding-bottom: 40px;
-        }
-
-    </style>
-
+    </div> <br>
+    <div class = "fInfo"> Copyright ${ new Date().getFullYear() } FutureDays Software </div>
     </footer>`
 }
