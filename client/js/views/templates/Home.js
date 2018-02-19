d2b88a2a0c8a4426260892baafc84a486dd94e55
Alex Cadigan <cadiganalexander@gmail.com>
2018-02-19 00:01:15 -0500

    Removed unnecessary comments

--- client/js/views/templates/Home.js
+++ client/js/views/templates/Home.js
@@ -1,8 +1,1 @@
-/**
-* Project: Future Days Farm Website
-* Created By: Chris Baron
-* Date Last Modified: 1/19/2018 by Alex Cadigan
-* Description: This file builds the body of the page (the main content)
-*/
-
 module.exports = function( { model } ) { return `<div>Home page stuff goes here</div>` }
