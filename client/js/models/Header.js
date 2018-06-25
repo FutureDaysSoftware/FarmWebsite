55b2b5f9f18de45cb784ed2db7fd1bbe39633050
AlexCadigan <cadiganalexander@gmail.com>
2018-06-25 15:23:25 -0400

    Updated code
    Modified code to use spread syntax instead of `Object.assign()`

--- client/js/models/Header.js
+++ client/js/models/Header.js
@@ -1,9 +1,3 @@
-module.exports = Object.assign({}, require('./__proto__.js'), {
-    data: [
-        'about us',
-        'where to find us',
-        'future days farm',
-        'the blog',
-        'our offerings'
-    ]
-})
+module.exports = { ...require('./__proto__.js'), 
+	data: ['about us', 'where to find us', 'future days farm', 'the blog', 'our offerings']
+}
