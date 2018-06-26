607c107e407278fb305f7b1f1b2c28ee37645428
AlexCadigan <cadiganalexander@gmail.com>
2018-06-26 14:14:53 -0400

    Updated name of Google Bucket

--- client/js/TemplateContext.js
+++ client/js/TemplateContext.js
@@ -101,7 +101,7 @@ module.exports = {
     IconDataJs( p ) { return p.name ? `data-js="${p.name}"` : `` },
 
     /* Eventually put the bucket name in the config file */
-    ImageSrc( name ) { return `https://storage.googleapis.com/five-gallon/${name}` },
+    ImageSrc( name ) { return `https://storage.googleapis.com/icelandic-heritage-chickens/${name}` },
 
     Range( int ) {
         return Array.from( Array( int ).keys() )
