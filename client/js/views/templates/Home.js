64b76c635f1d1bc3fdfc7a133ecf17fe85090229
AlexCadigan <cadiganalexander@gmail.com>
2018-07-02 18:44:28 -0400

    Restructured code to make it more efficient
    Made better use of the CSS flexbox

--- client/js/views/templates/Home.js
+++ client/js/views/templates/Home.js
@@ -1,12 +1,5 @@
 module.exports = function( { model } ) {
-	const images1 = model.imageMeta1.reduce((memo, image) => {
-	  	const markup = `<div>
-	    	<img data-src='${ this.ImageSrc(image.filename) }' alt='${ image.alt }'/> 
-	    	<div>${ image.date }<br/>${ image.caption }</div>
-	  	</div>`
-	  	return memo + markup
-	}, '')
-	const images2 = model.imageMeta2.reduce((memo, image) => {
+	const images = model.reduce((memo, image) => {
 	  	const markup = `<div>
 	    	<img data-src='${ this.ImageSrc(image.filename) }' alt='${ image.alt }'/> 
 	    	<div>${ image.date }<br/>${ image.caption }</div>
@@ -20,7 +13,6 @@ module.exports = function( { model } ) {
 	<div>Praesent laoreet ornare ligula, ac accumsan turpis sagittis at.  Integer auctor egestas eleifend. Etiam luctus 
 		mattis justo, vitae fermentum libero euismod lacinia. Proin at consequat risus.  Praesent sollicitudin 
 		vestibulum felis, ut sodales enim.</div>
-	<div>${ images1 }</div>
-	<div>${ images2 }</div>
+	<div class='calendar-images'>${ images }</div>
 	</div>`
 }
