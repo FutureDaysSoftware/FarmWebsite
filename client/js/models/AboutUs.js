25e7917bdb664a81cc97bbb4157144c0fcf0c0fd
AlexCadigan <cadiganalexander@gmail.com>
2018-07-06 13:57:01 -0400

    Changed ambiguous field name

--- client/js/models/AboutUs.js
+++ client/js/models/AboutUs.js
@@ -1,9 +1,9 @@
 module.exports = { ...require('./__proto__.js'),
 	data: [
-		{class: 'arrow', filename: 'LeftArrow.png', alt: 'Left Arrow', date: '', caption: ''},
-		{class: 'calendar-image', filename: 'Jam.jpg', alt: 'Jam', date: 'Nov. 12', caption: 'Wild Berry Jam'},
-		{class: 'calendar-image', filename: 'Beets.jpg', alt: 'Beets', date: 'Oct. 4', caption: 'We Got The Beets'},
-		{class: 'calendar-image', filename: 'Squash.jpg', alt: 'Squash', date: 'Oct. 13', caption: "It's Time For Squash"},
-		{class: 'arrow', filename: 'RightArrow.png', alt: 'Right Arrow', date: '', caption: ''}
+		{className: 'arrow', filename: 'LeftArrow.png', alt: 'Left Arrow', date: '', caption: ''},
+		{className: 'calendar-image', filename: 'Jam.jpg', alt: 'Jam', date: 'Nov. 12', caption: 'Wild Berry Jam'},
+		{className: 'calendar-image', filename: 'Beets.jpg', alt: 'Beets', date: 'Oct. 4', caption: 'We Got The Beets'},
+		{className: 'calendar-image', filename: 'Squash.jpg', alt: 'Squash', date: 'Oct. 13', caption: "It's Time For Squash"},
+		{className: 'arrow', filename: 'RightArrow.png', alt: 'Right Arrow', date: '', caption: ''}
 	]
 }
