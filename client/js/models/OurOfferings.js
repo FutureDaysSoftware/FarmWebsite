eb72cfc64ec6fbd2a0eeaba4c355dd3322ee30b1
AlexCadigan <cadiganalexander@gmail.com>
2018-07-11 14:24:34 -0400

    Draft of Our Offerings page

---
+++ client/js/models/OurOfferings.js
@@ -0,0 +1,17 @@
+module.exports = { ...require('./__proto__.js'),
+	data: [
+		{header: 'fall 2017', item: '', quantity: '', price: ''},
+		{header: '', item: 'Carrots', quantity: 'Bushel', price: '$ 1.50'},
+		{header: '', item: 'Kale', quantity: '1 lb.', price: '$ 3.00'},
+		{header: '', item: 'Eggs', quantity: 'A Dozen', price: '$ 6.25'},
+		{header: '', item: 'Magic Beans', quantity: 'Item Qty.', price: '$ 3.00'},
+		{header: 'year-round', item: '', quantity: '', price: ''},
+		{header: '', item: 'Praesent', quantity: '30', price: '$ 1.50'},
+		{header: '', item: 'Scelerisque', quantity: '1 Gallon', price: '$ 3.00'},
+		{header: '', item: 'Consectetur', quantity: 'Bushel', price: '$ 6.25'},
+		{header: '', item: 'Nullam', quantity: '1 lb.', price: '$ 3.00'},
+		{header: '', item: 'Vestibulum', quantity: 'A Dozen', price: '$ 3.00'},
+		{header: '', item: 'Duis mollis', quantity: 'Item Qty.', price: '$ 6.25'},
+		{header: '', item: 'Blacinia', quantity: 'A lot of them', price: '$ 3.00'}
+	]
+}
--- client/js/views/templates/OurOfferings.js
+++ client/js/views/templates/OurOfferings.js
@@ -1,6 +1,16 @@
 module.exports = function( { model } ) {
+	const offerings = model.reduce((memo, offer) => {
+		const markup = `<div>
+			<h3>${ offer.header }</h3>
+			<div>${ offer.item }</div>
+			<div>${ offer.quantity }</div>
+			<div>${ offer.price }</div>
+		</div>`
+		return memo + markup
+	}, '')
     return `<div>
     <h1>Headline for List of Stuff that’s Available</h1>
     <p>Ar egestas eleifend. Etiam luctus mattis justo, vitae fermentum libero euismod lacinia. Proin at consequat risus. Praesent sollicitudin vestibu.</p>
+    <div>${ offerings }</div>
     </div>`
 }
