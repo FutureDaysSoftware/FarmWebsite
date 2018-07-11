46eae0c4283cebabc0dd81b261f34b1c23406c9b
AlexCadigan <cadiganalexander@gmail.com>
2018-07-11 10:07:56 -0400

    Saving work

--- client/js/views/templates/OurOfferings.js
+++ client/js/views/templates/OurOfferings.js
@@ -1,3 +1,6 @@
 module.exports = function( { model } ) {
-    return `<div>our offerings</div>`
+    return `<div>
+    <h1>Headline for List of Stuff that’s Available</h1>
+    <p>Ar egestas eleifend. Etiam luctus mattis justo, vitae fermentum libero euismod lacinia. Proin at consequat risus. Praesent sollicitudin vestibu.</p>
+    </div>`
 }
