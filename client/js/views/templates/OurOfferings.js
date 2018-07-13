e57b54a32c467ad9b910eb731ce96aa773a12e3a
AlexCadigan <cadiganalexander@gmail.com>
2018-07-13 14:58:24 -0400

    Modified use of margins and flex

--- client/js/views/templates/OurOfferings.js
+++ client/js/views/templates/OurOfferings.js
@@ -1,16 +1,28 @@
 module.exports = function( { model } ) {
-	const offerings = model.reduce((memo, offer) => {
+	const fallOfferings = model.fall.reduce((memo, offer) => {
 		const markup = `<div>
-			<h3>${ offer.header }</h3>
 			<div>${ offer.item }</div>
 			<div>${ offer.quantity }</div>
-			<div>${ offer.price }</div>
+			<div>${ offer.price.toLocaleString('en', { style: 'currency', currency: 'USD' }) }</div>
+		</div>`
+		return memo + markup
+	}, '')
+	const yearRoundOfferings = model.yearRound.reduce((memo, offer) => {
+		const markup = `<div>
+			<div>${ offer.item }</div>
+			<div>${ offer.quantity }</div>
+			<div>${ offer.price.toLocaleString('en', { style: 'currency', currency: 'USD' }) }</div>
 		</div>`
 		return memo + markup
 	}, '')
     return `<div>
     <h1>Headline for List of Stuff that’s Available</h1>
     <p>Ar egestas eleifend. Etiam luctus mattis justo, vitae fermentum libero euismod lacinia. Proin at consequat risus. Praesent sollicitudin vestibu.</p>
-    <div>${ offerings }</div>
+    <div>
+    	<h3>fall 2017</h3>
+    	<div>${ fallOfferings }</div>
+    	<h3>year-round</h3>
+    	<div>${ yearRoundOfferings }</div>
+    </div>
     </div>`
 }
