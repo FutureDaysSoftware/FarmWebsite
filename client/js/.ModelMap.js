e052bb23856575bb10238abd4424862a9a9ee2ba
AlexCadigan <cadiganalexander@gmail.com>
2018-07-18 15:54:33 -0400

    Resolving merge conflicts

--- client/js/.ModelMap.js
+++ client/js/.ModelMap.js
@@ -2,6 +2,7 @@ module.exports={
 	 AboutUs: require('./models/AboutUs'),
 	Header: require('./models/Header'),
 	Home: require('./models/Home'),
+	OurOfferings: require('./models/OurOfferings'),
 	TheBlog: require('./models/TheBlog'),
 	User: require('./models/User'),
 	WhereToFindUs: require('./models/WhereToFindUs') 
