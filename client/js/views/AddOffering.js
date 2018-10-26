cbb5f8b74d80c59e072fc74f5099470e0ba04d42
Alex Cadigan <cadiganalexander@gmail.com>
2018-10-25 22:19:02 -0400

    Fixed formatting

--- client/js/views/AddOffering.js
+++ client/js/views/AddOffering.js
@@ -1,15 +1,13 @@
 module.exports = Object.assign({}, require('./__proto__'), {
     events: {
-        submit: 'click',
+        submit: 'click'
     },
 
     async onSubmitClick(e) {
         e.preventDefault()
-
         if (this.els.name.value.trim() == "") {
-            return 
+            return
         }
-
         const mutation = this.gql`mutation addTwoProducts($input: ProductInput!, $input2: ProductInput!) {
             firstAdd: addProduct(input: $input) {
                 id
@@ -20,23 +18,22 @@ module.exports = Object.assign({}, require('./__proto__'), {
         }`
         try {
             const result = await this.apolloClient.mutate({
-                mutation, 
-                variables: {
+                mutation, variables: {
                     input: {
                         name: this.els.name.value,
-                        quantity: this.els.quantity.value,
+                        quantity: this.els.quantity.value
                     },
                     input2: {
                         name: "Test",
-                        quantity: 1,
+                        quantity: 1
                     }
-                },
+                }
             })
             console.log(result)
         } 
         catch (e) {
             console.log(e)
-            this.Toast.showMessage( 'error', '...')
+            this.Toast.showMessage('error', '...')
         }
     }
 })
